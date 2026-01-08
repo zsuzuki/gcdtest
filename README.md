f95dea77e8c9cb91392f4c8e1ad49608d2cffc22
zsuzuki <wave.suzuki.z@gmail.com>
2026-01-09 03:26:09 +0900

    最初。

---
+++ README.md
@@ -0,0 +1,18 @@
+# gcdtest
+
+C++20 で MacOSの Grand Central Dispatch を試すテストです。
+
+MacOSの機能なので、MacOSでしかビルド・動作しません。
+
+## Build
+
+```sh
+cmake -S . -B build
+cmake --build build
+```
+
+## Run
+
+```sh
+./build/gcdtest
+```
---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+//
+//  Created by Yoshinori Suzuki on 2026
+//
+#include <dispatch/dispatch.h>
+#include <format>
+#include <iostream>
+#include <unistd.h>
+
+//
+template <class... Args>
+void Print(std::format_string<Args...> format, Args... args) {
+  std::cout << "\x1b[33m" << std::format(format, std::forward<Args>(args)...)
+            << "\x1b[0m\n";
+}
+
+//
+class Semaphore {
+  dispatch_semaphore_t sem;
+
+public:
+  Semaphore(int num = 1) : sem(dispatch_semaphore_create(num)) {}
+  ~Semaphore() { dispatch_release(sem); }
+
+  void lock() { dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER); }
+  void unlock() { dispatch_semaphore_signal(sem); }
+};
+
+//
+class Group {
+  dispatch_group_t group;
+  dispatch_queue_t queue;
+
+public:
+  Group() : group(dispatch_group_create()) {}
+  ~Group() { dispatch_release(group); }
+
+  void init(const char *label) {
+    queue = dispatch_queue_create(label, DISPATCH_QUEUE_CONCURRENT);
+  }
+
+  void async(auto func) {
+    dispatch_group_async(group, queue, ^{
+      func();
+    });
+  }
+
+  void notify(auto func) {
+    dispatch_group_notify(group, queue, ^{
+      func();
+    });
+  }
+
+  void wait() { dispatch_group_wait(group, DISPATCH_TIME_FOREVER); }
+};
+
+//
+int main() {
+  Print("Hello, C++20!");
+
+  Semaphore sem{5};
+  Group group;
+  group.init("com.example.myqueue");
+
+  for (int i = 0; i < 10; ++i) {
+    group.async([i, &sem]() {
+      sem.lock();
+      Print("Task {} started", i + 1);
+      sleep(2 + i % 3);
+      Print("Task {} completed", i + 1);
+      sem.unlock();
+    });
+  }
+  group.notify([&]() { Print("All tasks completed"); });
+
+  Print("Waiting for tasks to complete...");
+
+  group.wait();
+
+  return 0;
+}
